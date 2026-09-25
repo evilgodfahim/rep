2173d8d1a8f3b6236cb132eed43c6d162922699c
Fahim <49230752+evilgodfahim@users.noreply.github.com>
2026-09-25 20:45:50 +0600

    Update main1.py

--- main1.py
+++ main1.py
@@ -106,7 +106,7 @@ KL_API_FEEDS = set()
 
 # -- CONFIG --------------------------------------------------------------------
 
-GEMINI_MODEL = "gemini-3.5-flash-lite"
+MISTRAL_MODEL = "gemini-3.5-flash-lite"
 
 SEEN_FILE = "seen.json"
 SELECTED_FILE = "econ_selected_articles.json"
